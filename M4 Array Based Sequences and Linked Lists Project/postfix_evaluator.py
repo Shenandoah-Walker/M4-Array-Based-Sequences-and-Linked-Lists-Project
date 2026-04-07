643fa3503e903a6fd74a27a346cbc53ad75b3bdf
Shenandoah_Walker <saw67402@email.vccs.edu>
2026-04-07 12:53:55 -0400

    Wokred om postfix_evaluator.py

--- M4 Array Based Sequences and Linked Lists Project/postfix_evaluator.py
+++ M4 Array Based Sequences and Linked Lists Project/postfix_evaluator.py
@@ -1,9 +1,43 @@
 #Postfix Evaluator
 
-from stack import stack
+from stack import Stack
 
 class PostfixEvaluator:
 
     def __init__(self):
+        self.stack = Stack()
+
+    def evaluate_postfix(self, postfix_expression):
+        #Reset the stack for each evaluation
+        self.stack = Stack()  
+
+        characters = postfix_expression.split()
+
+        for character in characters:
+            #If the character is a number, push it onto the stack
+            if character.isdigit(): 
+                self.stack.push(int(character))
+            #In any other case, the character is an operator, so pop the top two operands, apply the operator, and push the result back onto the stack
+            else:  
+                right_number = self.stack.pop()  
+                left_number = self.stack.pop()
+
+                #Determine which 
+                if character == '+':
+                    self.stack.push(left_number + right_number)
+
+                elif character == '-':
+                    self.stack.push(left_number - right_number)
+
+                elif character == '*':
+                    self.stack.push(left_number * right_number)
+
+                elif character == '/':
+                    self.stack.push(left_number / right_number)
+
+                else:
+                    raise ValueError(f"The following operator is not basic arithmetic and therefore cannot be evaluated: {character}")
+
+            return self.stack.pop()
 
     
