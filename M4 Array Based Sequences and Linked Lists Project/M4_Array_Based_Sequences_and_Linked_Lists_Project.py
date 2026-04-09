c87f9b68227b8790cf44099af9c357d442d5eba2
Shenandoah_Walker <saw67402@email.vccs.edu>
2026-04-09 15:18:07 -0400

    Created backward list

--- M4 Array Based Sequences and Linked Lists Project/M4_Array_Based_Sequences_and_Linked_Lists_Project.py
+++ M4 Array Based Sequences and Linked Lists Project/M4_Array_Based_Sequences_and_Linked_Lists_Project.py
@@ -20,3 +20,21 @@ print("Delete the last node: Head ->", " -> ".join(str(x) for x in my_list), "->
 my_list.remove(30)
 print("Delete the interior node: Head ->", " -> ".join(str(x) for x in my_list), "-> None")
 
+#Backward list
+print()
+print("---- Build a backward list ----")
+my_list2 = SinglyLinkedList()
+my_list2.build_backward_list([10, 20, 30, 40, 50])
+print("Head ->", " -> ".join(str(x) for x in my_list2), "-> None")
+
+#Delete the first node
+my_list2.remove(50)
+print("Delete the first node: Head ->", " -> ".join(str(x) for x in my_list2), "-> None")
+
+#Delete the last node
+my_list2.remove(10)
+print("Delete the last node: Head ->", " -> ".join(str(x) for x in my_list2), "-> None")
+
+#Delete the interior node
+my_list2.remove(30)
+print("Delete the interior node: Head ->", " -> ".join(str(x) for x in my_list2), "-> None")
