9a49a76842f6d8b5f4c94d68146828c606afd32d
Shenandoah_Walker <saw67402@email.vccs.edu>
2026-04-09 15:16:12 -0400

    Created forward node part of test project

--- M4 Array Based Sequences and Linked Lists Project/M4_Array_Based_Sequences_and_Linked_Lists_Project.py
+++ M4 Array Based Sequences and Linked Lists Project/M4_Array_Based_Sequences_and_Linked_Lists_Project.py
@@ -1,3 +1,22 @@
 #Singly Linked List Test Program
 
-from singly_linked_list
+from singly_linked_list import SinglyLinkedList
+
+#Forward list
+print("---- Build a forward list ----")
+my_list = SinglyLinkedList()
+my_list.build_forward_list([10, 20, 30, 40, 50])
+print("Head ->", " -> ".join(str(x) for x in my_list), "-> None")
+
+#Delete the first node
+my_list.remove(10)
+print("Delete the first node: Head ->", " -> ".join(str(x) for x in my_list), "-> None")
+
+#Delete the last node
+my_list.remove(50)
+print("Delete the last node: Head ->", " -> ".join(str(x) for x in my_list), "-> None")
+
+#Delete the interior node
+my_list.remove(30)
+print("Delete the interior node: Head ->", " -> ".join(str(x) for x in my_list), "-> None")
+
