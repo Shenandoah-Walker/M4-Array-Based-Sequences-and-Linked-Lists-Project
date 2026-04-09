9734c8edc1dbc5ec5093e9e7ad8b92b9dff918aa
Shenandoah_Walker <saw67402@email.vccs.edu>
2026-04-09 13:46:26 -0400

    Wrote part of test program (Postfix Evaluation part)

--- M4 Array Based Sequences and Linked Lists Project/M4_Array_Based_Sequences_and_Linked_Lists_Project.py
+++ M4 Array Based Sequences and Linked Lists Project/M4_Array_Based_Sequences_and_Linked_Lists_Project.py
@@ -1,1 +1,25 @@
-#Infix and Postfix Expressions Project
+#Infix and Postfix Expressions Project Test Program
+
+import postfix_evaluator
+from stack import Stack
+from postfix_evaluator import PostfixEvaluator
+from infix_converter import InfixConverter
+
+#Test Data for Postfix Evaluation from Canvas:
+postfix = ["5 3 +",
+           "8 2 - 3 +",
+           "5 3 8 * +",
+           "6 2 / 3 +",
+           "5 8 + 3 -",
+           "5 3 + 8 *",
+           "8 2 3 * + 6 -",
+           "5 3 8 * + 2 /",
+           "8 2 + 3 6 * -",
+           "5 3 + 8 2 / -"]
+
+
+print("----- Postfix Evaluator -----")
+postfix_evaluator = PostfixEvaluator()
+for expression in postfix:
+    expression_evaluated = postfix_evaluator.evaluate_postfix(expression)
+    print(f"{expression} = {expression_evaluated}")
