09765bf5c037cb568d4d315d720f12b010e0e854
Shenandoah_Walker <saw67402@email.vccs.edu>
2026-04-14 14:05:57 -0400

    Wrote all code for split_evens_odds.py

--- M4 Array Based Sequences and Linked Lists Project/split_evens_odds.py
+++ M4 Array Based Sequences and Linked Lists Project/split_evens_odds.py
@@ -2,9 +2,51 @@ from singly_linked_list import SinglyLinkedList
 from singly_linked_list import Node
 
 class SplitEvensOdds(SinglyLinkedList):
-    def split_even_odd(self)):
+    def split_even_odd(self):
         #Raise exception if list is empty
-        if self.__head is None:
+        if self.SinglyLinkedList__head is None:
             raise SinglyLinkedList.EmptyListException()
 
         #Create even_list and odd_list as SinglyLinkedList objects
+        even_list = SinglyLinkedList()
+        odd_list = SinglyLinkedList()
+
+        #Assign current cursor to list.head
+        current = self._SinglyLinkedList__.head
+
+        #Clear original list
+        self._SinglyLinkedList__.head = None
+        self.SinglyLinkedList__.tail = None
+        self.SinglyLinkedList__.count = 0
+
+        #while (more nodes)
+        while current:
+            next_node = current.next
+            current.next = None #(disconnect node from original list)
+
+            #if current.data is even
+            if current.data % 2 == 0:
+                #if even_list.tail is None
+                if even_list._SinglyLinkedList__tail is None:
+                    even_list._SinglyLinkedList__head = current
+                    even_list._SinglyLinkedList__tail = current
+                
+                else:
+                    even_list._SinglyLinkedList__tail.next = current
+                    even_list._SinglyLinkedList__tail = current
+
+                even_list._SinglyLinkedList__count += 1
+
+            else:
+                #Node value is odd
+                if odd_list._SinglyLinkedList__tail is None:
+                    odd_list._SinglyLinkedList__head = current
+                    odd_list._SinglyLinkedList__tail = current
+                else:
+                    odd_list._SinglyLinkedList__tail.next = current
+                    odd_list._SinglyLinkedList__tail = current
+                odd_list._SinglyLinkedList__count += 1
+
+            current = next_node
+
+        return even_list, odd_list
