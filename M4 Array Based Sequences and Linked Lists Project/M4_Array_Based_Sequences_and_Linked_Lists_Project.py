53f5868315eb68e769ec5eb5a168148aedfcb67e
Shenandoah_Walker <saw67402@email.vccs.edu>
2026-04-14 14:37:28 -0400

    Wrote test program for SplitEvensOdds class

--- M4 Array Based Sequences and Linked Lists Project/M4_Array_Based_Sequences_and_Linked_Lists_Project.py
+++ M4 Array Based Sequences and Linked Lists Project/M4_Array_Based_Sequences_and_Linked_Lists_Project.py
@@ -1,73 +1,25 @@
-#Singly Linked List Test Program
+#Split Evens-Odds Test Program
 
 from singly_linked_list import SinglyLinkedList
+from split_evens_odds import SplitEvensOdds
 
-#Forward list
-print("---- Build a forward list ----")
-my_list = SinglyLinkedList()
-my_list.build_forward_list([10, 20, 30, 40, 50])
+print("---- Split Evens and Odds Test ----")
+
+#Build the original list
+my_list = SplitEvensOdds()
+my_list.build_forward_list([1, 2, 3, 4, 5, 6, 7, 8, 15, 14, 13, 12, 11, 10, 9])
+
+#Display original list
 print("Head ->", " -> ".join(str(x) for x in my_list), "-> None")
 
-#Delete the first node
-my_list.remove(10)
-print("Delete the first node: Head ->", " -> ".join(str(x) for x in my_list), "-> None")
+#Split the list
+evens, odds = my_list.split_even_odd()
 
-#Delete the last node
-my_list.remove(50)
-print("Delete the last node: Head ->", " -> ".join(str(x) for x in my_list), "-> None")
+#Display evens list
+print("Head ->", " -> ".join(str(x) for x in evens), "-> None")
 
-#Delete the interior node
-my_list.remove(30)
-print("Delete the interior node: Head ->", " -> ".join(str(x) for x in my_list), "-> None")
+#Display odds list
+print("Head ->", " -> ".join(str(x) for x in odds), "-> None")
 
-#Backward list
-print()
-print("---- Build a backward list ----")
-my_list2 = SinglyLinkedList()
-my_list2.build_backward_list([10, 20, 30, 40, 50])
-print("Head ->", " -> ".join(str(x) for x in my_list2), "-> None")
-
-#Delete the first node
-my_list2.remove(50)
-print("Delete the first node: Head ->", " -> ".join(str(x) for x in my_list2), "-> None")
-
-#Delete the last node
-my_list2.remove(10)
-print("Delete the last node: Head ->", " -> ".join(str(x) for x in my_list2), "-> None")
-
-#Delete the interior node
-my_list2.remove(30)
-print("Delete the interior node: Head ->", " -> ".join(str(x) for x in my_list2), "-> None")
-
-#Remove all test
-print()
-print("---- Remove all test ----")
-my_list3 = SinglyLinkedList()
-my_list3.build_forward_list([1, 2, 4, 6, 1, 3, 6])
-print("Head ->", " -> ".join(str(x) for x in my_list3), "-> None")
-
-#Remove all the occurrences of 1 in the list. (There should be 2)
-my_list3.remove_all(1)
-print("Removing 1 and all duplicates: Head ->", " -> ".join(str(x) for x in my_list3), "-> None")
-
-#Remove all the occurrences of 6 in the list. (There should be 2)
-my_list3.remove_all(6)
-print("Removing 6 and all duplicates: Head ->", " -> ".join(str(x) for x in my_list3), "-> None")
-
-#Non-recursive reverse display test
-print()
-print("---- Non-recursive reverse display test ----")
-
-my_list4 = SinglyLinkedList()
-my_list4.build_forward_list([10, 20, 30, 40, 50])
-
-#Insertion order
-print("Insertion order: Head ->", " -> ".join(str(x) for x in my_list4), "-> None")
-
-#Reverse order (recursive)
-print("Reverse order (recursive): None <-", end= ' ')
-my_list4.display_reverse()
-print(" <- Head")
-
-#Reverse order (non-recursive)
-my_list4.display_reverse_nr()
+#Display original list (should now be empty)
+print("Head ->", " -> ".join(str(x) for x in my_list), "-> None")
